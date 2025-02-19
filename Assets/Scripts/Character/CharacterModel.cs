512c4aae22a47d52e64af5a629aa7e082525b599
TaeHeon RYoo <taroni55@naver.com>
2025-02-19 16:24:46 +0900

    [Feat] 캐릭터 모델 변수 추가

--- Assets/Scripts/Character/CharacterModel.cs
+++ Assets/Scripts/Character/CharacterModel.cs
@@ -8,8 +8,30 @@ using UnityEngine;
 [System.Serializable]
 public class CharacterModel
 {
-    [SerializeField] string _name;
-    public string Name { get { return _name; } set { _name = value; } } 
+    [Header("Status")]
+    [SerializeField] string _characterName;
+    public string CharacterName { get { return _characterName; } set { _characterName = value; } }
+
+    [SerializeField] int _maxHp;
+    public int MaxHp { get { return _maxHp; } set { _maxHp = value; } }
+
+    [SerializeField] int _curHp;
+    public int CurHp { get { return _curHp; } set { _curHp = value; } }
+
+    [SerializeField] int _maxMana;
+    public int MaxMana { get { return _maxMana; } set { _maxMana = value; } }
+
+    [SerializeField] int _curMana;
+    public int CurMana { get { return _curMana; } set { _curMana = value; } }
+
+    [SerializeField] int _defense;
+    public int Defense { get { return _defense; } set { _defense = value; } }
+
+
+
+    [Header("Weapon")]
+    [SerializeField] string _weaponName;
+    public string WeaponName { get { return _weaponName; } set { _weaponName = value; } }
 
     [SerializeField] Vector2 _attackRange;
     public Vector2 AttackRange { get { return _attackRange; } set { _attackRange = value; } }
@@ -20,6 +42,21 @@ public class CharacterModel
     [SerializeField] int _damage;
     public int Damage { get { return _damage; } set { _damage = value; } }
 
-    [SerializeField] int _mana;
-    public int Mana { get { return _mana; } set { _mana = value; } }
+    [SerializeField] string _weaponAnimName;
+    public string WeaponAnimName { get { return _weaponAnimName; } set { _weaponAnimName = value; } }
+
+
+
+    [Header("Skill")]
+    [SerializeField] string _skillName;
+    public string SkillName { get { return _skillName; } set { _skillName = value; } }
+
+    [SerializeField] string _coolTime;
+    public string CoolTime { get { return _coolTime; } set { _coolTime = value; } }
+
+    [SerializeField] bool _canUse;
+    public bool CanUse { get { return _canUse; } set { _canUse = value; } }
+
+    [SerializeField] int _cost;
+    public int Cost { get { return _cost; } set { _cost = value; } }
 }
