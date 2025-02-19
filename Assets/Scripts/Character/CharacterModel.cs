bc8c035bac58dc85c559eefa6e9aa4058d09c591
TaeHeon RYoo <taroni55@naver.com>
2025-02-19 12:18:12 +0900

    [Feat] 캐릭터 모델 변수 추가

--- Assets/Scripts/Character/CharacterModel.cs
+++ Assets/Scripts/Character/CharacterModel.cs
@@ -8,9 +8,18 @@ using UnityEngine;
 [System.Serializable]
 public class CharacterModel
 {
+    [SerializeField] string _name;
+    public string Name { get { return _name; } set { _name = value; } } 
+
     [SerializeField] Vector2 _attackRange;
     public Vector2 AttackRange { get { return _attackRange; } set { _attackRange = value; } }
 
+    [SerializeField] int _attackDelay;
+    public int AttackDelay { get { return _attackDelay; } set { _attackDelay = value; } }
+
     [SerializeField] int _damage;
     public int Damage { get { return _damage; } set { _damage = value; } }
+
+    [SerializeField] int _mana;
+    public int Mana { get { return _mana; } set { _mana = value; } }
 }
