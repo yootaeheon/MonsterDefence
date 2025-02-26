c1958cba7349b1f64b1968df9049488d4e9431dd
RyooTaeHeon <taroni55@naver.com>
2025-02-26 11:43:10 +0900

    [Fix] 해금 시스템 로직 수정

--- Assets/Scripts/GameFlow/SceneChanger.cs
+++ Assets/Scripts/GameFlow/SceneChanger.cs
@@ -6,25 +6,47 @@ using System.Reflection;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public  class SceneChanger : MonoBehaviour
+public class SceneChanger : MonoBehaviour
 {
-   
-
     public static void ChangeScene(int index)
     {
         SceneManager.LoadScene(index);
     }
 
-    public  void SelectStage(int stageNum)
+    public void SelectStage(int stageNum)
     {
-        SceneManager.LoadScene(2);
+        GameManager.Instance.ChallengeStage = stageNum;
 
+        GameManager.Instance.MonsterCount = stageNum * 8;
+
+        if (!CheckEligibility(stageNum))
+        {
+            Debug.Log("아직 진행 불가");
+            return;
+        }
+
+        SceneManager.LoadScene(2);
         switch (stageNum)
         {
             case 1: GameManager.Instance.FilePath = "Map_01.csv"; break;
             case 2: GameManager.Instance.FilePath = "Map_02.csv"; break;
             case 3: GameManager.Instance.FilePath = "Map_03.csv"; break;
+        }
+
+
+    }
+
+    public bool CheckEligibility(int ChallengeStage)
+    {
+        if (ChallengeStage - DatabaseManager.Instance.CurStage  <= 1) //지금이 1 도전 2
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
